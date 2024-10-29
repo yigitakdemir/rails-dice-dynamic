6950a1b5a8355090d4635268270cd9e5805b6841
yigitakdemir <146138358+yigitakdemir@users.noreply.github.com>
2024-10-29 03:50:45 +0000

    5m 2250

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "dices", :action => "homepage"})
 
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "dices", :action => "flexible"})
 end
